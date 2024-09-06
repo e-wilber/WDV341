c080e2234f28feb83836861f16ff45f26ba5817e
E Wilber <157968704+e-wilber@users.noreply.github.com>
2024-09-05 19:40:23 -0500

    Add files via upload

---
+++ wdv341Homework.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>E. Wilber's Homework</title>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="about, student">
+    <meta name="description" content="Erin Wilber Assignments">
+</head>
+
+<style>
+    body {
+        background-color: darkslateblue;
+    }
+
+    .container {
+        background-color: gray;
+    }
+
+    * {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    body {
+        background-color: darkslateblue;
+    }
+
+    .container {
+        width: 700px;
+        margin: 0 auto;
+        background-color: gray;
+        box-shadow: 0 2px 5px 8px darkgray;
+    }
+
+    h1 {
+        font-size: 40px;
+        color: darkslateblue;
+        text-align: center;
+        margin-top: 10%;
+    }
+
+    h2 {
+        font-size: 20px;
+        color: darkslateblue;
+        text-align: center;
+    }
+
+    h3 {
+        color: darkslateblue;
+        margin: 5%;
+    }
+
+    ul {
+        color: white;
+        margin: 5%;
+    }
+
+    ul li a {
+        color: white;
+        font-size: 15px;
+    }
+
+    .container {
+        padding: 50px;
+    }
+
+    ul {
+        text-align: left;
+        padding-left: 0;
+    }
+
+    li {
+        text-decoration: none;
+        list-style: none;
+        font-size: 1.1rem;
+        line-height: 2rem;
+    }
+
+    .links {
+        color: white;
+        text-decoration: none;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .links a:hover {
+        color: black;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <h1><strong>PHP Assignments</strong></h1>
+
+        <h2>E. Wilber</h2>
+
+        <h2><i class="links"><a href="../home.html">Home Page</a></i></h2>
+
+        <h3><strong>Unit 1</strong></h3>
+        <ul>
+            <li class="links"><a href="wdv341Homework.php">Assignment: Create WDV341 Homework Page</a></li>
+            <li>1-1: Setting Up Localhost</li>
+            <li>1-2: Download Git Client & Create a Repository account</li>
+        </ul>
+
+        <h3><strong>Unit 2</strong></h3>
+        <ul>
+            <li>2-1: PHP Basics</a></li>
+            <li>PHP Basics Quiz</li>
+        </ul>
+
+        <h3><strong>Unit 3</strong></h3>
+        <ul>
+            <li>3-1 PHP Functions</li>
+            <li>PHP Functions Quiz</li>
+        </ul>
+
+        <h3><strong>Unit 4</strong></h3>
+        <ul>
+            <li>4-1: HTML Form Processor</li>
+        </ul>
+
+        <h3><strong>Unit 5</strong></h3>
+        <ul>
+            <li>5-1: Image File uploader</li>
+            <li>PROJECT: Contact Form with Email</li>
+        </ul>
+
+        <h3><strong>Unit 6</strong></h3>
+        <ul>
+            <li>6-1: Create wdv341 database and events table</li>
+            <li>6-2: Create a connection file</li>
+        </ul>
+
+        <h3><strong>Unit 7</strong></h3>
+        <ul>
+            <li>7-1: Create selectEvents.php</li>
+            <li>7-2: Create selectOneEvent.php</li>
+        </ul>
+        
+        <h3><strong>Unit 8</strong></h3>
+        <ul>
+            <li>8-1: PHP-JSON Event Object</li>
+        </ul>
+
+        <h3><strong>Unit 9</strong></h3>
+        <ul>
+            <li>PROJECT: PHP Formatted Dynamic Output</li>
+            <li>9-1: Input Form Events and INSERT into database</li>
+        </ul>
+
+        <h3><strong>Unit 10</strong></h3>
+        <ul>
+            <li>10-1: Create event_user table</li>
+            <li>10-2: Create a login.php page</li>
+            <li>10-3: Create a logout.php page</li>
+        </ul>
+
+        <h3><strong>Unit 11</strong></h3>
+        <ul>
+            <li>11-1: Create DELETE functionality</li>
+        </ul>
+
+        <h3><strong>Unit 12</strong></h3>
+        <ul>
+            <li>12-1: Create Update Form for an Event</li>
+        </ul>
+
+        <h3><strong>Unit 13</strong></h3>
+        <ul>
+            <li>13-1: Protect Form Processors</a></li>
+        </ul>
+
+        <h3><strong>FINAL: Portfolio Project</strong></h3>
+    </div>
+</body>
+
+</html>
