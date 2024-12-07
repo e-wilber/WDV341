beee0f662cec8a6abda0e786ebc321a2f32d8dee
E Wilber <157968704+e-wilber@users.noreply.github.com>
2024-12-06 18:53:39 -0600

    Error response page
    error response page provided by professor

---
+++ 505_error_response_page.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<html>
+
+<head>
+	<title>Sorry, there has been an error.</title>
+</head>
+
+
+
+<body>
+
+	<h1>We're sorry. Our system is having issues.  Please try again later.</h1>
+    
+    <p>Return to <a href="#">home page</a>.</p>
+
+
+</body>
+</html>
