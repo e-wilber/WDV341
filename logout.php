1b802af56216781d67ec8aa0c50c2c1732e0b3cb
E Wilber <157968704+e-wilber@users.noreply.github.com>
2024-12-08 01:46:19 -0600

    Assignment 10-3: Create a logout.php page
    completed assignment

--- logout.php
+++ logout.php
@@ -1,6 +1,10 @@
 <?php
-    $conn = null;
+//destroy session
+    session_unset();    //remove session variables
+    session_destroy();  //remove the session
 
+//disconnect from database
+    $conn = null;
     $stmt = null;
 
     //then redirect to the login page
