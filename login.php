34bd82fa97527e49ab69141f5f04555e24f5bc83
E Wilber <157968704+e-wilber@users.noreply.github.com>
2024-12-08 10:03:24 -0600

    Assignment 10-2: Create a login.php page
    completed assignment

--- login.php
+++ login.php
@@ -1,70 +1,83 @@
 <?php
+session_start();    //access to current session or starts new one if needed
+
 $errorMsg = "";
+
 //check if form was submitted or needs displayed to the customer
-if( isset($_POST["submit"])){
-    //did the user submit the signon form? then
-    //the form was submitted continue processing the form data
 
-    //get data from form, connect to database, see if there's a matching record in the user table
-    /*  
-    if(match = true){
-            valid user
-            display admin page
-        }  
-    else{
-        Invalid user
-        display error msg
-        display login form
-    } 
-    */
-    $inUsername = $_POST['inUsername'];   //pull username from form (case sensitive)
-    $inPassword = $_POST['inPassword'];   //pull password
-
-    //connect to database
-    try {
-        require 'dbConnect.php';    //Connects to the database
-        
-        //does the username and password match the one in the database?
-        //SELECT for a specific set of data using WHERE
-        //$sql = "SELECT user_username, user_password FROM wdv341_users WHERE user_username = :username AND user_password = :password";    
-        
-        $sql = "SELECT COUNT(*) FROM wdv341_users WHERE user_username = :username AND user_password = :password";
-        
-        //Prepared statement PDO
-        $stmt = $conn->prepare($sql);
-        
-        //Bind Parameters
-        $stmt->bindParam(":username", $inUsername);
-        $stmt->bindParam(":password", $inPassword);
-        
-        //Executes the PDO Statement and saves results in $stmt object
-        $stmt->execute();
-
-        $rowCount = $stmt->fetchColumn();
-
-        if($rowCount > 0){
-            //its a user listed in the database
-            //echo "<h3>Login Successful</h3>";
-            $validUser = true;  //switch aka flag
-        }
-        else{
-            //invalid
-            //echo "<h3>NOT A VALID USER OR PASSWORD</h3>";
-            $validUser = false;
-            $errorMsg = "Invalid username or password. Enter again.";
-        }
-        
-        //Process the results from the query
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);  //return values as an ASSOC array
-
-    } catch (PDOException $e) {
-        echo "Database Failed: " . $e->getMessage();
-    }
+if( $_SESSION['validSession'] === "yes"){
+    //if you are a validSession then you should see admin page and stay signed on 
+    $validUser = true;  //sets valid user to display the admin page
 }
 else{
-    //the customer needs to see the form in order to fill it out and SUBMIT it for sign on
-}
+    //you need to sign on
 
+    if( isset($_POST["submit"])){
+        //did the user submit the signon form? then
+        //the form was submitted continue processing the form data
+
+        //get data from form, connect to database, see if there's a matching record in the user table
+        /*  
+        if(match = true){
+                valid user
+                display admin page
+            }  
+        else{
+            Invalid user
+            display error msg
+            display login form
+        } 
+        */
+        $inUsername = $_POST['inUsername'];   //pull username from form (case sensitive)
+        $inPassword = $_POST['inPassword'];   //pull password
+
+        //connect to database
+        try {
+            require 'dbConnect.php';    //Connects to the database
+            
+            //does the username and password match the one in the database?
+            //SELECT for a specific set of data using WHERE
+            //$sql = "SELECT user_username, user_password FROM wdv341_users WHERE user_username = :username AND user_password = :password";    
+            
+            $sql = "SELECT COUNT(*) FROM wdv341_users WHERE user_username = :username AND user_password = :password";
+            
+            //Prepared statement PDO
+            $stmt = $conn->prepare($sql);
+            
+            //Bind Parameters
+            $stmt->bindParam(":username", $inUsername);
+            $stmt->bindParam(":password", $inPassword);
+            
+            //Executes the PDO Statement and saves results in $stmt object
+            $stmt->execute();
+
+            $rowCount = $stmt->fetchColumn();
+
+            if($rowCount > 0){
+                //its a user listed in the database
+                //echo "<h3>Login Successful</h3>";
+                $validUser = true;  //switch aka flag
+                $_SESSION['validSession'] = "yes";  //Valid user with access to Admin pages
+            }
+            else{
+                //invalid
+                //echo "<h3>NOT A VALID USER OR PASSWORD</h3>";
+                $validUser = false;
+                $errorMsg = "Invalid username or password. Enter again.";
+                $_SESSION['validSession'] = "no";  //Invalid user
+            }
+            
+            //Process the results from the query
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);  //return values as an ASSOC array
+
+        } catch (PDOException $e) {
+            echo "Database Failed: " . $e->getMessage();
+        }
+    }
+    else{
+        //the customer needs to see the form in order to fill it out and SUBMIT it for sign on
+    }
+}   //end of check for validSession
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -93,7 +106,7 @@ else{
             <h3>OPTIONS</h3>
 
             <ol>
-                <li><a href="eventInputForm.html">Add New Event</li>
+                <li><a href="eventInputForm.php">Add New Event</li>
                 <li><a href="selectEvents.php">Display Events</li>
                 <li><a href="logout.php">Logout</li>
             </ol>
