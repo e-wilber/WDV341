f0655e7fd7c9b37d04d83a2e04c187b5bfd75f46
E Wilber <157968704+e-wilber@users.noreply.github.com>
2024-12-07 20:29:40 -0600

    Assignment 10-2: Create a login.php
    2nd commit, still not finished

--- login.php
+++ login.php
@@ -1,7 +1,48 @@
 <?php
 //check if form was submitted or needs displayed to the customer
 if( isset($_POST["submit"])){
+    //did the user submit the signon form? then
     //the form was submitted continue processing the form data
+
+    //get data from form, connect to database, see if there's a matching record in the user table
+    /*  
+    if(match = true){
+            valid user
+            display admin page
+        }  
+    else{
+        Invalid user
+        display error msg
+        display login form
+    } 
+    */
+    $username = $_POST['inUsername'];   //pull username from form (case sensitive)
+    $password = $_POST['inPassword'];   //pull password
+
+    //connect to database
+    try {
+        require 'dbConnect.php';    //Connects to the database
+        
+        //does the username and password match the one in the database?
+        //SELECT for a specific set of data using WHERE
+        $sql = "SELECT user_username, user_password FROM wdv341_users WHERE user_username = :username AND user_password = :password";                   
+        
+        //Prepared statement PDO
+        $stmt = $conn->prepare($sql);
+        
+        //Bind Parameters
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":password", $password);
+        
+        //Executes the PDO Statement and saves results in $stmt object
+        $stmt->execute();
+        
+        //Process the results from the query
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);  //return values as an ASSOC array
+
+    } catch (PDOException $e) {
+        echo "Database Failed: " . $e->getMessage();
+    }
 }
 else{
     //the customer needs to see the form in order to fill it out and SUBMIT it for sign on
@@ -47,8 +88,7 @@ else{
                 <p>
                     <input type="submit" name="submit" value="submit">
                     <input type="reset">
-                </p>
-                
+                </p>   
             </form>
         </section>
     <?php
